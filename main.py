692ae3b897647b9527edcae0bb14193ce612c7ba
KyleM73 <kyle.morgenstein@gmail.com>
2020-09-27 11:57:28 -0700

    added shapes and fonts

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from PIL import Image
 import pathlib
+import tarfile
+import tempfile
+import requests
+
 #first demo
 #for i in range(10):
 #    fname = "image"+str(i)+".jpg"
@@ -11,14 +15,45 @@ img.show("Background")
 print("Image Size:")
 print(img.size)
 print("\n")
+
 w = h = 512
 
-save_dir = pathlib.Path("./images").expanduser()
-save_dir.mkdir(exist_ok=True,parents=True)
+save_dir_crops = pathlib.Path("./images").expanduser()
+save_dir_crops.mkdir(exist_ok=True,parents=True)
 
 for x in range(0,img.size[0]-w,w):
     for y in range(0,img.size[1]-h,h):
         crop = img.crop((x,y,x+w,y+h))
-        crop.save(save_dir / f"crop_{x}_{y}.jpg")
+        crop.save(save_dir_crops / f"crop_{x}_{y}.jpg")
 print("Crops Generated:")
-print(len(list(save_dir.glob("*.jpg"))))
+print(len(list(save_dir_crops.glob("*.jpg"))))
+
+url = "https://bintray.com/uavaustin/target-finder-assets/download_file?file_path=base-shapes-v1.tar.gz"
+
+save_dir_assets = pathlib.Path("./assets").expanduser()
+if not save_dir_assets.is_dir():
+    save_dir_assets.mkdir(exist_ok=True,parents=True)
+
+res = requests.get(url,stream=True)
+
+with tempfile.TemporaryDirectory() as d:
+    tmp_file = pathlib.Path(d) / "file.tar.gz"
+    tmp_file.write_bytes(res.raw.read())
+    
+    with tarfile.open(tmp_file) as tar:
+        tar.extractall(save_dir_assets)
+
+shape = Image.open("./assets/base-shapes-v1/circle/circle-01.png")
+shape.show("Shape")
+
+url_fonts = "https://bintray.com/uavaustin/target-finder-assets/download_file?file_path=fonts.tar.gz"
+
+res_fonts = requests.get(url_fonts,stream=True)
+
+with tempfile.TemporaryDirectory() as d:
+    tmp_file = pathlib.Path(d) / "file.tar.gz"
+    tmp_file.write_bytes(res_fonts.raw.read())
+
+    with tarfile.open(tmp_file) as tar:
+        tar.extractall(save_dir_assets)
+
