614842f81d8f99198352aff54a83740138a9eac1
KyleM73 <kyle.morgenstein@gmail.com>
2020-09-27 11:34:51 -0700

    changed file to crop documented image

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from PIL import Image
-for i in range(10):
-    fname = "image"+str(i)+".jpg"
-    im = Image.new(mode="RGB",size=(512,512),color=(int(255/(i+1)),int(255/(i+1)),int(255/(i+1))))
-    im.save("./images/"+fname)
-    
+import pathlib
+#first demo
+#for i in range(10):
+#    fname = "image"+str(i)+".jpg"
+#    im = Image.new(mode="RGB",size=(512,512),color=(int(255/(i+1)),int(255/(i+1)),int(255/(i+1))))
+#    im.save("./images/"+fname)
+
+img = Image.open("./background.jpg")
+img.show("Background")
+print("Image Size:")
+print(img.size)
+print("\n")
+w = h = 512
+
+save_dir = pathlib.Path("./images").expanduser()
+save_dir.mkdir(exist_ok=True,parents=True)
+
+for x in range(0,img.size[0]-w,w):
+    for y in range(0,img.size[1]-h,h):
+        crop = img.crop((x,y,x+w,y+h))
+        crop.save(save_dir / f"crop_{x}_{y}.jpg")
+print("Crops Generated:")
+print(len(list(save_dir.glob("*.jpg"))))
